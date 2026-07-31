74e15e8b9f16bd69aff964ba7e840f78e9604ead
Liana <lianamoradi97@gmail.com>
2026-07-31 05:05:02 -0700

    lesson05: loops

---
+++ lesson05_loops.py
@@ -0,0 +1,10 @@
+name = input('What is your name?')
+print(f"Welcome {name}")
+for i in  range(1, 6):
+    print(f"Student {i}: Hello {name}")
+
+for i in range(2, 10, 2):
+    print(i)
+
+print(f"Thank you {name}")
+print("Good bye")
